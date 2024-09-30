edbe4357e30adb53961d0bb91b55a99f38e09745
Tishalovich Maxim <tishalovichm@gmail.com>
2024-09-30 07:44:00 +0300

    Add filter for tasks

--- src/pages/TasksPage.tsx
+++ src/pages/TasksPage.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { urlBase } from "../config/config"
-import { Task } from "../model/task"
+import { Task, TaskStatus } from "../model/task"
 import TaskPreview from "./components/TaskPreview"
 import { PageType } from "./PageType"
 
@@ -9,8 +9,16 @@ type TaskPageProps = {
     setPageType: (pageType: PageType) => void
 }
 
+enum StatusFilter {
+    NONE,
+    DONE,
+    IN_PROGRESS,
+    TDDO,
+}
+
 function TasksPage(props: TaskPageProps) {
     const [tasks, setTasks] = useState([])
+    const [filter, setFilter] = useState<TaskStatus | undefined>(undefined)
 
     const selectTask = props.selectTask
     const setPageType = props.setPageType
@@ -34,18 +42,45 @@ function TasksPage(props: TaskPageProps) {
         )
     }
 
+    const filteredTasks = tasks.filter(
+        (t: Task) => filter === undefined || t.status == filter
+    )
+
+    const onFilterChange = (newFilterStr: string) => {
+        if (newFilterStr == "undefined") {
+            setFilter(undefined)
+        } else {
+            setFilter(Number.parseInt(newFilterStr))
+        }
+    }
+
     return (
         <>
             <h1>
                 Tasks
             </h1>
+            <div>
+                <h2>
+                    Filter:
+                </h2>
+                <select
+                    value={filter}
+                    onChange={e => onFilterChange(e.target.value)}
+                >
+                    <option value="undefined">None</option>
+                    <option value="0">TODO</option>
+                    <option value="1">In progress</option>
+                    <option value="2">Done</option>
+                </select>
+            </div>
             {
-                tasks.map((t: Task) =>
-                    TaskPreview(t, ()=>{
-                        selectTask(t)
-                        setPageType(PageType.Task)
-                    }
-                )) 
+                filteredTasks
+                    .map((t: Task) =>
+                        TaskPreview(t, ()=>{
+                            selectTask(t)
+                            setPageType(PageType.Task)
+                        }
+                    )) 
             }
         </>
     )
